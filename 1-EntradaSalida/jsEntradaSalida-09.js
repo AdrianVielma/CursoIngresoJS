6b88d4331298f207058be7a426aafd5118365edb
adrianochox <adrianochox@gmail.com>
2023-02-04 00:30:24 -0300

    4/2/23

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+//declaro variables
+	var importeIngresado;
+	var	resultado;
+	var
+
+	importeIngresado = document.getElementById(txtIdImporte);
+	resultado = document.getElementById(txtIdResultado);
+
+	//parseo
+	importeIngresado = parseInt(importeIngresado)
+
+
 
 	alert("ok");
 }
